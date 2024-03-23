afc4fa6af2fa71decb2521bfcc5f2bfb8e515a08
Luíza Nurnberg <luiza.nurnberg@allog.com.br>
2024-03-23 12:37:51 -0300

    feat(color): add variable color to describe

--- src/test/unit/validate-vehicle-color.spec.ts
+++ src/test/unit/validate-vehicle-color.spec.ts
@@ -6,7 +6,7 @@ describe("validateVehicleColor", () => {
   describe("GIVEN a vehicle with correct color", () => {
     it("SHOULD pass through validator and returns true", () => {
       const isColorValid = validateVehicleColor(
-        correctColorVehicle.license_plate
+        correctColorVehicle.color
       );
 
       expect(isColorValid).toBe(true);
@@ -16,7 +16,7 @@ describe("validateVehicleColor", () => {
   describe("GIVEN a vehicle with a wrong color ", () => {
     it("SHOULD NOT pass through validator and returns false", () => {
       const isColorValid = validateVehicleColor(
-        wrongColorVehicle.license_plate
+        wrongColorVehicle.color
       );
 
       expect(isColorValid).toBe(false);
