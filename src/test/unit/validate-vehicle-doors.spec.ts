89cc203a5e98b940933972c2867d1114a036bf1e
maariaceciliaholler <mariaceciliaholler@gmail.com>
2024-04-02 18:08:32 -0300

    feat(vehicle-doors): adjusted description for door

--- src/test/unit/validate-vehicle-doors.spec.ts
+++ src/test/unit/validate-vehicle-doors.spec.ts
@@ -2,20 +2,20 @@ import validateVehicleDoors from "../../usecase/validate-vehicle-doors.usecase";
 import arrangeTestContext from "./arrange-test-context";
 
 
-describe("validateVehicleWeight", () => {
+describe("validateVehicleDoors", () => {
  const { wrongNumbersOfDoorsVehicle, correctNumbersOfDoorsVehicle } = arrangeTestContext();
- describe("GIVEN a vehicle with correct weight", () => {
+ describe("GIVEN a vehicle with correct Doors", () => {
    it("SHOULD pass through validator and returns true", () => {
-     const isWeightValid = validateVehicleDoors(correctNumbersOfDoorsVehicle);
-     expect(isWeightValid).toBe(true);
+     const isDoorsValid = validateVehicleDoors(correctNumbersOfDoorsVehicle);
+     expect(isDoorsValid).toBe(true);
    });
  });
 
 
- describe("GIVEN a vehicle with a wrong weight", () => {
+ describe("GIVEN a vehicle with a wrong Doors", () => {
    it("SHOULD NOT pass through validator and returns false", () => {
-     const isWeightValid = validateVehicleDoors(wrongNumbersOfDoorsVehicle);
-     expect(isWeightValid).toBe(false);
+     const isDoorsValid = validateVehicleDoors(wrongNumbersOfDoorsVehicle);
+     expect(isDoorsValid).toBe(false);
    });
  });
 });
